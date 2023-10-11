fdca35f5dbc31e55e8feb8f136b38ff3cb1cbb03
Adam Children <adam.children@themix.org.uk>
2023-10-11 22:32:45 +0100

    feat: Add footer

--- top-recruiter/app/page.tsx
+++ top-recruiter/app/page.tsx
@@ -3,6 +3,7 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import CallToActionWithIllustration from './components/Home/hero'
 import NewsletterCTA from './components/Newsletter/newsletter'
 import HomeCardList from './components/Home/cards'
+import Footer from './components/Footer/footer'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HomeCardList />
 
       <NewsletterCTA/>
+      <Footer />
     </main>
   )
 }
