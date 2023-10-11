cee73971ede5537e2e50033564703f77b94520d1
Adam Children <adam.children@themix.org.uk>
2023-10-11 22:21:34 +0100

    feat: update cta to orange

--- top-recruiter/app/components/Navigation/nav.tsx
+++ top-recruiter/app/components/Navigation/nav.tsx
@@ -77,10 +77,10 @@ export default function WithSubnavigation() {
                         fontSize={'sm'}
                         fontWeight={600}
                         color={'white'}
-                        bg={'pink.400'}
+                        bg={'orange.400'}
                         href={'#'}
                         _hover={{
-                            bg: 'pink.300',
+                            bg: 'orange.500',
                         }}>
                         Sign Up
                     </Button>
@@ -151,12 +151,12 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
             display={'block'}
             p={2}
             rounded={'md'}
-            _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
+            _hover={{ bg: useColorModeValue('orange.300', 'gray.900') }}>
             <Stack direction={'row'} align={'center'}>
                 <Box>
                     <Text
                         transition={'all .3s ease'}
-                        _groupHover={{ color: 'pink.400' }}
+                        _groupHover={{ color: 'white' }}
                         fontWeight={500}>
                         {label}
                     </Text>
@@ -170,7 +170,7 @@ const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
                     justify={'flex-end'}
                     align={'center'}
                     flex={1}>
-                    <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+                    <Icon color={'white'} w={5} h={5} as={ChevronRightIcon} />
                 </Flex>
             </Stack>
         </Box>
