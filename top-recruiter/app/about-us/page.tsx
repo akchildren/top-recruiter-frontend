2b2b032441d7b11379043664938f6dafa1cc5702
Adam Children <adam.children@themix.org.uk>
2023-10-11 13:34:23 +0100

    fate: home init

---
+++ top-recruiter/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+
+export default function AboutUs() {
+  return (
+    <main>
+      
+
+    </main>
+  )
+}
